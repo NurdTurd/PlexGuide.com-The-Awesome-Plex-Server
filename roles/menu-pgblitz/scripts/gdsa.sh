0d9ae42663bc8809eb61ccfc0c831897e6258e57
Admin9705 <user@dunn.cloud>
2018-10-05 23:16:27 -0400

    update

--- roles/menu-pgblitz/scripts/gdsa.sh
+++ roles/menu-pgblitz/scripts/gdsa.sh
@@ -15,67 +15,6 @@
 #   under the GPL along with build & install instructions.
 #
 #################################################################################
-json=$(cat /var/plexguide/json.tempbuild)
-
-mkdir -p /opt/pgops 1>/dev/null 2>&1
-mkdir -p /mnt/tdrive/plexguide/checks 1>/dev/null 2>&1
-tdrive=$( cat /root/.config/rclone/rclone.conf | grep team_drive | head -n1 )
-tdrive="${tdrive:13}"
-
-echo
-echo "Validating $json"
-
-mkdir -p /mnt/tdrive/plexguide/checks 1>/dev/null 2>&1
-
-echo ""
-echo "Testing JSON: $json"
-
-rm -r /root/.config/rclone/rclone.tmp 1>/dev/null 2>&1
-
-tee "/root/.config/rclone/rclone.tmp" > /dev/null <<EOF
-[GDSATEST]
-type = drive
-client_id =
-client_secret =
-scope = drive
-root_folder_id =
-service_account_file = /opt/appdata/pgblitz/keys/processed/$json
-team_drive = $tdrive
-EOF
-
-mkdir -p /opt/pgops/GDSATEST
-touch /opt/pgops/GDSATEST/$json
-
-rclone move --tpslimit 6 --checkers=20 \
-  --config /root/.config/rclone/rclone.tmp \
-  --transfers=8 \
-  --log-file=/opt/appdata/pgblitz/rclone.log --log-level INFO --stats 10s \
-  --exclude="**partial~" --exclude="**_HIDDEN~" \
-  --exclude=".unionfs-fuse/**" --exclude=".unionfs/**" \
-  --drive-chunk-size=32M \
-  /opt/pgops/GDSATEST GDSATEST:plexguide/checks && rclone_fin_flag=1
-
-echo "Waiting 3 Seconds"
-sleep 3
-
-temp=$((rclone lsf --config /root/.config/rclone/rclone.tmp \
-GDSATEST:plexguide/checks/ | grep -o -P "25" ) 2>&1)
-
-temp2=$(echo $temp | grep -oP "error" | head -c 5)
-
-  if [ "$temp2" == "error" ]; then
-    RED='\033[0;31m'
-    NC='\033[0m'
-    echo -e "JSON: $json - Failed - ${RED}INVALID${NC}"
-    rm -r /mnt/tdrive/plexguide/checks/$json 1>/dev/null 2>&1
-    else
-      GREEN='\033[0;32m'
-      NC='\033[0m'
-      echo -e "JSON: $json - ${GREEN}VALID${NC}"
-      bash /opt/plexguide/roles/menu-pgblitz/scripts/gdsa.sh
-  fi
-
-#####################
 downloadpath=$(cat /var/plexguide/server.hd.path)
 tempbuild=$(cat /var/plexguide/json.tempbuild)
 path=/opt/appdata/pgblitz/keys
